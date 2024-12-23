9d6b2cce3ea6ef1fbb48895c1d2c3bf3c3ea53d5
Kovrizhkin-ya <kovrizhkinaleksandr95@gmail.com>
2024-12-23 12:39:52 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,203 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import uploadImage from './browse.png';
+import browseIcon from './upload.svg';
+import downloadImage from './download.png';
+
+const url = process.env.REACT_APP_API_URL;
+
+const statusProgressMapping = {
+  'Extracting staffline and symbols': 5,
+  'Extracting layers of different symbols': 10,
+  'Extracting noteheads': 15,
+  'Analyzing notehead bboxes': 20,
+  'Instantiating notes': 25,
+  'Grouping noteheads': 30,
+  'Extracting symbols': 35,
+  'Extracting rhythm types': 40,
+  'Building MusicXML document': 75,
+  'File is ready': 100
+};
+
+function App() {
+  const [file, setFile] = useState(null);
+  const [isUploaded, setIsUploaded] = useState(false);
+  const [processing, setProcessing] = useState(false);
+  const [downloadUrl, setDownloadUrl] = useState(null);
+  const [progress, setProgress] = useState(0);
+  const [fileId, setFileId] = useState(null);
+  const [statusMessage, setStatusMessage] = useState('');
+
+  const handleFileDrop = (event) => {
+    event.preventDefault();
+    const droppedFile = event.dataTransfer.files[0];
+    setFile(droppedFile);
+    setIsUploaded(true);
+  };
+
+  const handleBrowseClick = () => {
+    document.getElementById('fileInput').click();
+  };
+
+  const handleFileChange = (event) => {
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
+    setIsUploaded(true);
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  const handleConvert = () => {
+    if (file) {
+      uploadFile(file);
+    }
+  };
+
+  const uploadFile = async (file) => {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    const response = await fetch(`${url}/api/sendFile`, {
+      method: 'POST',
+      body: formData,
+      headers: {
+        "ngrok-skip-browser-warning": "any-value"
+      }
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      setFileId(data.file_id);
+      checkFileStatus(data.file_id);
+    } else {
+      console.error('File upload failed.');
+    }
+  };
+
+  const checkFileStatus = async (fileId) => {
+    setProcessing(true);
+
+    const interval = setInterval(async () => {
+      const response = await fetch(`${url}/api/checkFileStatus/${fileId}`, {
+     headers: {
+          "ngrok-skip-browser-warning": "any-value"
+        } 
+      });
+
+      if (response.ok) {
+        const text = await response.text();
+        const data = JSON.parse(text);
+        const message = data.message;  
+
+        setStatusMessage(message); 
+
+        if (statusProgressMapping[message] !== undefined) {
+          setProgress(statusProgressMapping[message]);
+        }  
+
+        if (message === 'File is ready') {
+          clearInterval(interval);
+          setProcessing(false);
+          setDownloadUrl(`${url}/api/getFile/${fileId}`);
+        } else if (message === 'Failed') {
+          clearInterval(interval);
+          setProcessing(false);
+          console.error('File processing failed.');
+        }
+      } else {
+        console.error('Failed to check file status.');
+      }
+    }, 5000);
+  };
+
+  const handleDownload = () => {
+    if (downloadUrl) {
+      const a = document.createElement('a');
+      a.href = downloadUrl;
+      a.download = 'file';
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }
+  };
+
+
+  const handleReset = () => {
+    setFile(null);
+    setIsUploaded(false);
+    setProcessing(false);
+    setDownloadUrl(null);
+    setProgress(0);
+    setFileId(null);
+    setStatusMessage('');
+  };
+
+  return (
+    <div className="App">
+      <div
+        className="upload-container"
+        onDrop={handleFileDrop}
+        onDragOver={handleDragOver}
+      >
+        {!downloadUrl && !isUploaded && (
+          <div className="upload-box">
+            <img src={uploadImage} alt="Upload" className="upload-image" />
+            <p className="upload-text">Drop, Upload, or Paste file</p>
+            <p className="upload-supported">Supported formats: PNG, JPG, JFIF (JPEG), GIF</p>
+            <button className="browse-btn" onClick={handleBrowseClick}>
+              <img src={browseIcon} alt="Browse" className="browse-icon" />
+              <span className="browse-text">Browse</span>
+            </button>
+            <input
+              id="fileInput"
+              type="file"
+              style={{ display: 'none' }}
+              onChange={handleFileChange}
+            />
+          </div>
+        )}
+
+        {isUploaded && !downloadUrl && !processing && (
+          <div className="convert-box">
+            <p className="convert-near-text">{file.name}</p>
+            <button className="convert-btn" onClick={handleConvert}>
+              Convert
+            </button>
+          </div>
+        )}
+
+        {processing && (
+          <div className="progress-block">
+            <div className="progress-message">
+              <p className="status-message">{statusMessage}</p>  
+            </div>
+            <div className="progress-container">
+              <div className="progress-bar" style={{ width: `${progress}%` }}>
+                {progress}%
+              </div>
+            </div>
+          </div>
+        )}
+
+        {downloadUrl && !processing && (
+          <div className="download-container" onClick={handleDownload}>
+            <img src={downloadImage} alt="Download" className="download-image" />
+            <span className="download-link">Download</span>
+          </div>
+        )}
+
+        {downloadUrl && (
+          <div className="reset-link-container">
+            <a href="#" className="reset-link" onClick={handleReset}>
+              New upload
+            </a>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
